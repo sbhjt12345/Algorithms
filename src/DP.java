536e095e8ab79c63a56f07d22827a22d1ef0a8fb
Jiantao Hu <jh2468@cornell.edu>
2017-03-10 23:00:29 +0900

    add

--- src/DP.java
+++ src/DP.java
@@ -97,4 +97,22 @@ public class DP {
 	   }
 	   return dp[0][n-1];
    }
+   
+   public boolean canPartition(int[] nums) {
+	   /**
+	    * Just some knapsack problem
+	    * **/
+       int sum = 0;
+       for (int num : nums) sum += num;
+       if (sum%2!=0) return false;
+       sum /=2;
+       boolean[] dp = new boolean[sum+1];
+       dp[0] = true;
+       for (int num : nums){
+           for (int j= sum;j>=num;j--){
+               dp[j] = dp[j] || dp[j-num];
+           }
+       }
+       return dp[sum];
+    }
 }
